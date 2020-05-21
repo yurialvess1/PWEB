729b3cbb8a0f12dcccd2039545db06e78d3cf37a
yurialvess1 <50206711+yurialvess1@users.noreply.github.com>
2020-05-20 21:55:15 -0300

    Add files via upload

---
+++ Atividade7/CalculaArea.js
@@ -0,0 +1,7 @@
+function CalculaArea(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var area = num1*num2;
+
+    alert("A Area e: "+area);
+   }
