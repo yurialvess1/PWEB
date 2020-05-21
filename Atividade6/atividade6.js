3615bf1808a10c13d5f7944f507ff14f6682da27
yurialvess1 <50206711+yurialvess1@users.noreply.github.com>
2020-05-20 21:53:40 -0300

    Add files via upload

---
+++ Atividade6/atividade6.js
@@ -0,0 +1,14 @@
+var Aluno1 = new Object;
+Aluno1.RA = "0030481713009";
+Aluno1.Nome = "Yuri";
+
+
+var Aluno2 = {
+    RA: "0030481713009",
+    Nome: "Yuri";
+}
+
+var Aluno3 = new Object();
+Aluno3['RA'] = '0030481713009'
+Aluno3['Nome'] = 'Yuri'
+
