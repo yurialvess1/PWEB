6b0042126e94bcb22afde2e198c7da27edb6fd62
yurialvess1 <50206711+yurialvess1@users.noreply.github.com>
2020-06-25 22:13:37 -0300

    Add files via upload

---
+++ Atividade8/assets/custom/js/script.js
@@ -0,0 +1,5 @@
+/*
+Encontre na rede 3 figuras: janela aberta, janela fechada e janela quebrada.
+
+Criar uma página que no carregamento mostra a janela fechada com um título “Abra a Janela”. Se mover com o mouse sobre a imagem da janela fechada, mostra a janela aberta (abre a janela), se sair com o mouse da imagem mostra a janela fechada (fecha a janela). Se clicar  sobre a imagem mostra a imagem da janela quebrada (quebra a janela).
+*/
